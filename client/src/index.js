67b34b7418aa81cbddc29cc2979fb9c67ea97297
Colleen Handel <handelc22@gmail.com>
2021-02-25 13:24:54 -0600

    Added test AJAX request from client and refactored server as proxy server

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 console.log('helloworld')
 ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
+
+document.getElementById('test').addEventListener('click', () => {
+  console.log('clicked');
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:8080/atelier/products',
+    success: (data) => {
+      console.log('success!', data);
+    },
+    error: (err) => {
+      console.log('error!', err);
+    }
+  })
+})
--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require('express')
-const app = express()
-const port = 8080
-app.use(express.static('client/dist'))
+const app = express();
+const port = 8080;
+app.use(express.static('client/dist'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const API_SERVICE_URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/';
+const dotenv = require('dotenv');
+dotenv.config();
 
-  res.send()
-})
+app.use('/atelier', createProxyMiddleware({
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+      [`^/atelier`]: '',
+  },
+  onProxyReq: function onProxyReq(proxyReq) {
+    proxyReq.setHeader('Authorization', process.env.API_KEY);
+  }
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
