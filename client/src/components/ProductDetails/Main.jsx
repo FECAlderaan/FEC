e82ce7621640b2d135cfb137ece5e23bd70d41b7
Colleen Handel <handelc22@gmail.com>
2021-02-26 22:34:19 -0600

    Implemented Ratings component for Product Overview

--- client/src/components/ProductDetails/Main.jsx
+++ client/src/components/ProductDetails/Main.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating.jsx';
 
-const ProductDetails = () => {
-  return <h1>Product Details!</h1>
+const ProductDetails = ({productId}) => {
+  return (
+    <div id='productOverview'>
+      <h1>Product Details!</h1>
+      <Rating productId={productId}/>
+    </div>
+  )
+};
+
+ProductDetails.propTypes = {
+  productId: PropTypes.number
 }
 
 export default ProductDetails;
---
+++ client/src/components/ProductDetails/Rating.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import $ from 'jquery';
+
+class Rating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ratings: []
+    }
+  }
+
+  componentDidMount() {
+    this.getRating();
+  }
+
+  getRating(pageNum = 1) {
+    $.ajax({
+      type: 'GET',
+      url: `http://localhost:8080/atelier/reviews/?product_id=${this.props.productId}&count=1000&page=${pageNum}`,
+      success: (data) => {
+        this.setState({ratings: this.state.ratings.concat(data.results.map(review => review.rating))});
+        if (data.results.length === 1000) {
+          pageNum++;
+          this.getRating(pageNum);
+        }
+      },
+      error: (err) => {
+        console.log('error!', err);
+      }
+    });
+  }
+
+  render() {
+    var rating = this.state.ratings.reduce((aggRating, rating) => { return aggRating + rating }, 0) / (this.state.ratings.length || 1);
+    if (rating === 0) {
+      return null;
+    }
+    var ratingRoundedPercent = (Math.round(rating * 4) / 4) * 20;
+    return (
+    <div>
+      <div className='productOverview-rating'>
+        <div className='productOverview-rating-filled' style={{'width': ratingRoundedPercent}}>
+          <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+        </div>
+        <div className='productOverview-rating-empty'>
+          <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+        </div>
+      </div>
+      {/* might have to change this link depending on the id given to the ratings & reviews section */}
+      <a href='#reviews'>Read {this.state.ratings.length === 1 ? '1 review' : `all ${this.state.ratings.length} reviews`}</a>
+    </div>
+    )
+  }
+}
+
+Rating.propTypes = {
+  productId: PropTypes.number
+}
+
+export default Rating;
