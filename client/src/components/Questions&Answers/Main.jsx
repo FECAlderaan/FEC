bb08aafb3f822dc15bd9a53fe6f14ff2656d1a65
abachant <aubreybachant@gmail.com>
2021-03-01 19:13:09 -0500

    Refactor question-list into a new component

--- client/src/components/Questions&Answers/Main.jsx
+++ client/src/components/Questions&Answers/Main.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import QuestionList from './QuestionList.jsx'
 
 const QuestionAnswer = () => {
   return (
@@ -9,37 +9,7 @@ const QuestionAnswer = () => {
         <input type="text" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." size="128" />
         <input type="submit" value="Submit" />
       </form>
-      <ul className="question-list">
-        <div className="question-list-entry">
-          <div className="question">
-            <h3>Q: </h3>
-            <p>What is the deal with clothes?</p>
-            <div className="question-data">
-              <p>Helpful? <a href="">Yes</a> [2]</p>
-              <a href="">Add Answer</a>
-            </div>
-          </div>
-          <div className="answer-list">
-            <h3>A: </h3>
-            <div className="answer-entry">
-              <p>They are pretty good.</p>
-              <div className="answer-entry-data">
-                <p>by User1231, March 01 2021</p>
-                <p>Helpful? <a href="">Yes</a> [2]</p>
-                <a href="">Report</a>
-              </div>
-            </div>
-            <div className="answer-entry">
-              <p>They are pretty cool too.</p>
-              <div className="answer-entry-data">
-                <p>by Qunitillius42565 Seller, March 01 2021</p>
-                <p>Helpful? <a href="">Yes</a> [2]</p>
-                <a href="">Report</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </ul>
+      <QuestionList />
       <button>More Answered Questions</button>
       <button>Ask A Question + </button>
     </div>
