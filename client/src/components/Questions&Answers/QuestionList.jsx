92b8bf330d52b5ec5ae2bcb4659948199bd5d9eb
abachant <aubreybachant@gmail.com>
2021-03-02 18:21:58 -0500

    Make sure questions are rendered in order of helpfulness property

--- client/src/components/Questions&Answers/QuestionList.jsx
+++ client/src/components/Questions&Answers/QuestionList.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'underscore';
 import QuestionListEntry from './QuestionListEntry.jsx'
 
 const QuestionList = ({product}) => {
   const renderQuestions = () => {
     if (product) {
+      // Sort array of question objects by their value for question_helpfulness property
+      product.questions = _.sortBy(product.questions, 'question_helpfulness')
+      // Reverse the order of the questions so the most helpful is first
+      product.questions.reverse();
       return product.questions.map((question) => <QuestionListEntry question={question} key={question.question_id}/> )
     }
   }
@@ -16,7 +21,7 @@ const QuestionList = ({product}) => {
 }
 
 QuestionList.propTypes = {
-  product: PropTypes.array
+  product: PropTypes.object
 }
 
 export default QuestionList;
