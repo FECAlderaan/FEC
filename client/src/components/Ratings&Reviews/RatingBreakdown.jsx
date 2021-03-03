2d090701e683fa0652e5356dddd36d585a4ab9ee
Adrian Rodriguez <justpickmeanemailaddress@gmail.com>
2021-03-03 15:52:52 -0500

    Added filling bars on star ratings

--- client/src/components/Ratings&Reviews/RatingBreakdown.jsx
+++ client/src/components/Ratings&Reviews/RatingBreakdown.jsx
@@ -44,7 +44,20 @@ var getRecommended = function (recommended) {
 
 // Finding width for how many stars need to be filled in
 // 1 star = 20%
-var averagePercent = Number((Math.round(3.8 * 4) / 4).toFixed(2)) * 20;
+var averagePercent = function (object) {
+  return Number((Math.round(ratingAverage(object) * 4) / 4).toFixed(2)) * 20;
+}
+
+// Find percent to be filled in for star bars
+var starBars = function (stars, object) {
+  // get total number of reviews
+  var reviewsCount = Object.values(object);
+  var count = 0;
+  reviewsCount.map((rating) => {
+    count += Number(rating);
+  })
+  return Math.round(((stars/count) * 100) * 100) / 100;
+}
 
 const RatingBreakdown = (props) => {
   return (
@@ -58,7 +71,7 @@ const RatingBreakdown = (props) => {
           <i className="far fa-star"></i>
           <i className="far fa-star"></i>
           <i className="far fa-star"></i>
-          <div className="stars-inner" style={{ width: `${averagePercent}%` }}>
+          <div className="stars-inner" style={{ width: `${averagePercent(props.ratingData.ratings)}%` }}>
             <i className="fas fa-star"></i>
             <i className="fas fa-star"></i>
             <i className="fas fa-star"></i>
@@ -73,35 +86,35 @@ const RatingBreakdown = (props) => {
         <div className="rating-bar-section">
           <h4 className="star-header">5 stars</h4>
           <div className="rating-bar-outer">
-            <div className="rating-bar-inner" id="five-star-bar"></div>
+            <div className="rating-bar-inner" id="five-star-bar" style={{ width: `${starBars(props.ratingData.ratings[5] , props.ratingData.ratings)}%` }}></div>
           </div>
           <h4 className="individual-total-ratings">{props.ratingData.ratings[5] | 0}</h4>
         </div>
         <div className="rating-bar-section">
           <h4 className="star-header">4 stars</h4>
           <div className="rating-bar-outer">
-            <div className="rating-bar-inner" id="four-star-bar"></div>
+            <div className="rating-bar-inner" id="four-star-bar" style={{ width: `${starBars(props.ratingData.ratings[4] , props.ratingData.ratings)}%` }}></div>
           </div>
           <h4 className="individual-total-ratings">{props.ratingData.ratings[4] | 0}</h4>
         </div>
         <div className="rating-bar-section">
           <h4 className="star-header">3 stars</h4>
           <div className="rating-bar-outer">
-            <div className="rating-bar-inner" id="three-star-bar"></div>
+            <div className="rating-bar-inner" id="three-star-bar" style={{ width: `${starBars(props.ratingData.ratings[3] , props.ratingData.ratings)}%` }}></div>
           </div>
           <h4 className="individual-total-ratings">{props.ratingData.ratings[3] | 0}</h4>
         </div>
         <div className="rating-bar-section">
           <h4 className="star-header">2 stars</h4>
           <div className="rating-bar-outer">
-            <div className="rating-bar-inner" id="two-star-bar"></div>
+            <div className="rating-bar-inner" id="two-star-bar" style={{ width: `${starBars(props.ratingData.ratings[2] , props.ratingData.ratings)}%` }}></div>
           </div>
           <h4 className="individual-total-ratings">{props.ratingData.ratings[2] | 0}</h4>
         </div>
         <div className="rating-bar-section">
           <h4 className="star-header">1 stars</h4>
           <div className="rating-bar-outer">
-            <div className="rating-bar-inner" id="one-star-bar"></div>
+            <div className="rating-bar-inner" id="one-star-bar" style={{ width: `${starBars(props.ratingData.ratings[1] , props.ratingData.ratings)}%` }}></div>
           </div>
           <h4 className="individual-total-ratings">{props.ratingData.ratings[1] | 0}</h4>
         </div>
