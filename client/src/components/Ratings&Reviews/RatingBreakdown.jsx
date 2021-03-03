347844534da45c0674d00361bd8bac2826a5ddd6
Adrian Rodriguez <justpickmeanemailaddress@gmail.com>
2021-03-03 09:51:46 -0500

    Added styling

--- client/src/components/Ratings&Reviews/RatingBreakdown.jsx
+++ client/src/components/Ratings&Reviews/RatingBreakdown.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import ProductBreakdown from './ProductBreakdown.jsx';
 
-var getRatings = function(object) {
+var getRatings = function (object) {
   var array = [];
 
   for (const [key, value] of Object.entries(object)) {
@@ -14,7 +14,7 @@ var getRatings = function(object) {
 }
 
 // Get overall average of ratings
-var ratingAverage = function(object) {
+var ratingAverage = function (object) {
   var ratingsCount = Object.values(object);
   var average = 0;
   var count = 0;
@@ -30,22 +30,63 @@ var ratingAverage = function(object) {
   ratingsCount.map((rating) => {
     count += Number(rating);
   })
-  return (Math.round(average/count * 10) / 10).toString();
+  return (Math.round(average / count * 10) / 10).toString();
 }
+
 
 const RatingBreakdown = (props) => {
   return (
     <div className="rating-breakdown">
-      <p>Rating Breakdown</p>
-
+      <h2>Rating Breakdown</h2>
       <div className="rating-average">
         <h1 id="average-number">{ratingAverage(props.ratingData.ratings)}</h1>
         ★★★★☆
       </div>
 
-      <div className="rating-percent">2</div>
+      <div className="rating-stars">
 
-      <div className="rating-stars">3</div>
+        <div className="rating-bar-section">
+          <h4 className="star-header">5 stars</h4>
+          <div className="rating-bar-outer">
+            <div className="rating-bar-inner" id="five-star-bar"></div>
+          </div>
+          <h4 className="individual-total-ratings">{props.ratingData.ratings[5] | 0}</h4>
+        </div>
+        <div className="rating-bar-section">
+          <h4 className="star-header">4 stars</h4>
+          <div className="rating-bar-outer">
+            <div className="rating-bar-inner" id="four-star-bar"></div>
+          </div>
+          <h4 className="individual-total-ratings">{props.ratingData.ratings[4] | 0}</h4>
+        </div>
+        <div className="rating-bar-section">
+          <h4 className="star-header">3 stars</h4>
+          <div className="rating-bar-outer">
+            <div className="rating-bar-inner" id="three-star-bar"></div>
+          </div>
+          <h4 className="individual-total-ratings">{props.ratingData.ratings[3] | 0}</h4>
+        </div>
+        <div className="rating-bar-section">
+          <h4 className="star-header">2 stars</h4>
+          <div className="rating-bar-outer">
+            <div className="rating-bar-inner" id="two-star-bar"></div>
+          </div>
+          <h4 className="individual-total-ratings">{props.ratingData.ratings[2] | 0}</h4>
+        </div>
+        <div className="rating-bar-section">
+          <h4 className="star-header">1 stars</h4>
+          <div className="rating-bar-outer">
+            <div className="rating-bar-inner" id="one-star-bar"></div>
+          </div>
+          <h4 className="individual-total-ratings">{props.ratingData.ratings[1] | 0}</h4>
+        </div>
+
+      </div>
+
+      <div className="rating-percent">
+        100% of reviews recommend this product
+      </div>
+
       <ProductBreakdown />
     </div>
   )
