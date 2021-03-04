821e36ecedc5c6d35ecd210c9fc0bc8238d9a246
abachant <aubreybachant@gmail.com>
2021-03-04 17:38:25 -0500

    Update <AnswerListEntry /> to display date in Month DD, YYYY format

--- client/src/components/Questions&Answers/AnswerListEntry.jsx
+++ client/src/components/Questions&Answers/AnswerListEntry.jsx
@@ -5,12 +5,19 @@ const AnswerListEntry = ({ answer }) => {
   // Destructure and rename answerer_name into camelCase
   const { answerer_name: answererName } = answer;
   const { body, date, helpfulness } = answer;
+
+  // Convert date string into Month DD, YYYY format
+  const formatDate = (dateString) => {
+    // parse date from string
+    const dateObj = new Date(dateString);
+    return dateObj.toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' });
+  };
   return (
     <div className="answer-entry">
       <p>{body}</p>
       <div className="answer-entry-data">
         <p>
-          {`by ${answererName}, ${date}`}
+          {`by ${answererName}, ${formatDate(date)}`}
         </p>
         <p>
           Helpful?
