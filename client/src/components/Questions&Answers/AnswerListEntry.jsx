f79b18e9ddbe0693c86c96d4e833fa6686843f6b
abachant <aubreybachant@gmail.com>
2021-03-05 18:49:08 -0500

    Update markHelpful() to only mark as helpful once (per session)

--- client/src/components/Questions&Answers/AnswerListEntry.jsx
+++ client/src/components/Questions&Answers/AnswerListEntry.jsx
@@ -2,57 +2,72 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import $ from 'jquery';
 
-const AnswerListEntry = ({ answer }) => {
-  // Destructure and rename answerer_name into camelCase
-  const { answerer_name: answererName } = answer;
-  const { body, date, helpfulness, id } = answer;
-
-  // Convert date string into Month DD, YYYY format
-  const formatDate = (dateString) => {
-    // parse date from string
-    const dateObj = new Date(dateString);
-    return dateObj.toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' });
-  };
-
-  // If answer is from seller than render 'Seller' in bold
-  const checkIfSeller = (answerer) => {
-    if (answerer === 'Seller') {
-      return <b>{answerer}</b>;
-    }
-    return answerer;
-  };
-
-  const markHelpful = () => {
-    const route = `http://localhost:8080/atelier/qa/answers/${id}/helpful`;
-    $.ajax({
-      url: route,
-      method: 'PUT',
-    })
-      .fail((error) => console.log(error));
-  };
-
-  return (
-    <div className="answer-entry">
-      <p>{body}</p>
-      <div className="answer-entry-data">
-        <p>
-          {'by '}
-          {checkIfSeller(answererName)}
-          {', '}
-          {formatDate(date)}
-        </p>
-        <p>
-          Helpful?
-          <button type="button" onClick={markHelpful}>Yes</button>
-          (
-          {helpfulness}
-          )
-        </p>
-        <a href="/">Report</a>
-      </div>
-    </div>
-  );
+// Convert date string into Month DD, YYYY format
+const formatDate = (dateString) => {
+  // parse date from string
+  const dateObj = new Date(dateString);
+  return dateObj.toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric' });
 };
+
+// If answer is from seller than render 'Seller' in bold
+const checkIfSeller = (answerer) => {
+  if (answerer === 'Seller') {
+    return <b>{answerer}</b>;
+  }
+  return answerer;
+};
+
+class AnswerListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markedHelpful: false,
+    };
+    this.markHelpful = this.markHelpful.bind(this);
+  }
+
+  markHelpful() {
+    const { answer: { id } } = this.props;
+    const { markedHelpful } = this.state;
+    const route = `http://localhost:8080/atelier/qa/answers/${id}/helpful`;
+    if (!markedHelpful) {
+      $.ajax({
+        url: route,
+        method: 'PUT',
+      })
+        .success((this.setState({ markedHelpful: true })))
+        .fail((error) => console.log(error));
+    }
+  }
+
+  render() {
+    const { answer } = this.props;
+    // Destructure and rename answerer_name into camelCase
+    const { answerer_name: answererName } = answer;
+    const { body, date, helpfulness } = answer;
+    return (
+      <div className="answer-entry">
+        <p>{body}</p>
+        <div className="answer-entry-data">
+          <p>
+            {'by '}
+            {checkIfSeller(answererName)}
+            {', '}
+            {formatDate(date)}
+          </p>
+          <p>
+            Helpful?
+            <button type="button" onClick={this.markHelpful}>Yes</button>
+            (
+            {helpfulness}
+            )
+          </p>
+          <a href="/">Report</a>
+        </div>
+      </div>
+    );
+  }
+}
 
 AnswerListEntry.propTypes = {
   answer: PropTypes.instanceOf(Object).isRequired,
