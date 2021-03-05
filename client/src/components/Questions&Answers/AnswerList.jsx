12f96cfc28d47a3c31893724d65f43dd14ec6ff6
abachant <aubreybachant@gmail.com>
2021-03-05 17:02:35 -0500

    Add button to load or collapse answers

--- client/src/components/Questions&Answers/AnswerList.jsx
+++ client/src/components/Questions&Answers/AnswerList.jsx
@@ -8,9 +8,12 @@ class AnswerList extends React.Component {
     super(props);
     this.state = {
       entries: [],
+      showAll: false,
     };
     this.sortAnswers = this.sortAnswers.bind(this);
     this.arrangeAnswers = this.arrangeAnswers.bind(this);
+    this.toggleShowAll = this.toggleShowAll.bind(this);
+    this.renderDisplayButton = this.renderDisplayButton.bind(this);
     this.renderAnswers = this.renderAnswers.bind(this);
   }
 
@@ -42,22 +45,44 @@ class AnswerList extends React.Component {
     }
   }
 
+  toggleShowAll() {
+    const { showAll } = this.state;
+    this.setState({ showAll: !showAll });
+  }
+
+  // Render a button to toggle to load or collapse answers depending what is already rendered
+  renderDisplayButton(displayed) {
+    const { entries } = this.state;
+    const total = entries.length;
+    let displayButton;
+    // compare length of visible answers to length of total answers to determine button to render
+    if (displayed === total && total > 2) {
+      // only allow users to collapse answers if total length is greater than 2
+      displayButton = <button type="button" onClick={this.toggleShowAll}>Collapse answers</button>;
+    } else if (displayed < total) {
+      // only allow users to load answers if there are unloaded answers
+      displayButton = <button type="button" onClick={this.toggleShowAll}>See more answers</button>;
+    }
+    return displayButton;
+  }
+
   // Convert entries in state into <AnswerListEntry />s to be rendered
   renderAnswers() {
-    const { entries } = this.state;
+    const { entries, showAll } = this.state;
     // subset of entries to be displayed
     let display = [];
     // Handle if there are only 2 answers
     if (entries.length < 3) {
       display = [...entries];
-    } else {
+    } else if (!showAll) {
       display = [entries[0], entries[1]];
+    } else {
+      display = [...entries];
     }
     // Only show 2 answers and a load more button
     return [
       display.map((answer) => <AnswerListEntry answer={answer} key={answer.id} />),
-      // only add button if there are more entries left to be displayed
-      (display.length < entries.length ? <button type="button">See more answer</button> : ''),
+      this.renderDisplayButton(display.length),
     ];
     // renders objects that aresupplied to it
   }
