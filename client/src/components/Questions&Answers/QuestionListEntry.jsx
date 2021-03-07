c348b1d0db679a42cff5d14ef7812f2c432120fc
abachant <aubreybachant@gmail.com>
2021-03-06 22:37:04 -0500

    Refactor <QuestionListEntry /> into a class component

--- client/src/components/Questions&Answers/QuestionListEntry.jsx
+++ client/src/components/Questions&Answers/QuestionListEntry.jsx
@@ -3,41 +3,47 @@ import PropTypes from 'prop-types';
 import AnswerList from './AnswerList';
 import AnswerModal from './AnswerModal';
 
-const QuestionListEntry = ({ question }) => {
-  // Destructure and rename question_helpfulness into camelCase
-  const { question_helpfulness: questionHelpfulness, question_body: questionBody } = question;
-  const { answers } = question;
+class QuestionListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   // Convert answers from an object to an array;
-  const parseAnswers = () => {
+  parseAnswers() {
+    const { question: {answers} } = this.props;
     let answerList;
     if (answers) {
       answerList = Object.entries(answers).map((answer) => answer[[1]]);
     }
     return answerList;
-  };
+  }
 
-  return (
-    <div className="question-list-entry">
-      <div className="question">
-        <h3>Q: </h3>
-        <p>{questionBody}</p>
-        <div className="question-data">
-          <p>
-            Helpful?
-            <a href="/"> Yes </a>
-            (
-            {questionHelpfulness}
-            )
-          </p>
-          <button type="button">Add Answer</button>
+  render() {
+    const question = this.props;
+    // Destructure and rename question_helpfulness into camelCase
+    const { question_helpfulness: questionHelpfulness, question_body: questionBody } = question;
+    return (
+      <div className="question-list-entry">
+        <div className="question">
+          <h3>Q: </h3>
+          <p>{questionBody}</p>
+          <div className="question-data">
+            <p>
+              Helpful?
+              <a href="/"> Yes </a>
+              (
+              {questionHelpfulness}
+              )
+            </p>
+            <button type="button">Add Answer</button>
+          </div>
+          <AnswerModal />
         </div>
-        <AnswerModal />
+        <AnswerList answers={this.parseAnswers()} />
       </div>
-      <AnswerList answers={parseAnswers()} />
-    </div>
-  );
-};
+    )
+  }
+}
 
 QuestionListEntry.propTypes = {
   question: PropTypes.instanceOf(Object).isRequired,
