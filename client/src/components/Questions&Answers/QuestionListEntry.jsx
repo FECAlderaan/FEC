d98b2c47720b80c061e815f474fdf7801c24e65d
abachant <aubreybachant@gmail.com>
2021-03-07 22:30:33 -0500

    Add functionality to mark question as helpful

--- client/src/components/Questions&Answers/QuestionListEntry.jsx
+++ client/src/components/Questions&Answers/QuestionListEntry.jsx
@@ -1,17 +1,23 @@
+/* eslint-disable no-console */
 import React from 'react';
 import PropTypes from 'prop-types';
+import $ from 'jquery';
 import AnswerList from './AnswerList';
 import AnswerModal from './AnswerModal';
 
 class QuestionListEntry extends React.Component {
   constructor(props) {
     super(props);
+    const { question } = props;
     this.state = {
       showAnswerModal: false,
+      markedHelpful: false,
+      helpfulness: question.question_helpfulness,
     };
-
     this.parseAnswers = this.parseAnswers.bind(this);
     this.toggleAnswerModal = this.toggleAnswerModal.bind(this);
+    this.markHelpful = this.markHelpful.bind(this);
+    this.renderHelpfulButton = this.renderHelpfulButton.bind(this);
     this.renderAnswerModal = this.renderAnswerModal.bind(this);
   }
 
@@ -30,6 +36,33 @@ class QuestionListEntry extends React.Component {
     this.setState({ showAnswerModal: !showAnswerModal });
   }
 
+  // Increase a question's helpfulness property
+  markHelpful() {
+    const { question: { question_id: id } } = this.props;
+    const { markedHelpful, helpfulness } = this.state;
+    const route = `http://localhost:8080/atelier/qa/questions/${id}/helpful`;
+    if (!markedHelpful) {
+      $.ajax({
+        url: route,
+        method: 'PUT',
+      })
+        // update state so render can reflect new value
+        .done(this.setState({ markedHelpful: true, helpfulness: helpfulness + 1 }))
+        .fail((error) => console.log(error));
+    }
+  }
+
+  // Render a button that can only be clicked once for marking a helpful answer
+  renderHelpfulButton() {
+    const { markedHelpful } = this.state;
+    let helpfulButton = <button type="button" onClick={this.markHelpful}>Yes</button>;
+    if (markedHelpful) {
+      helpfulButton = <span>Yes</span>;
+    }
+    return helpfulButton;
+  }
+
+  // Render a modal for submitting an answer
   renderAnswerModal() {
     const { showAnswerModal } = this.state;
     const { question: { question_id: questionId } } = this.props;
@@ -41,9 +74,9 @@ class QuestionListEntry extends React.Component {
   }
 
   render() {
-    const question = this.props;
-    // Destructure and rename question_helpfulness into camelCase
-    const { question_helpfulness: questionHelpfulness, question_body: questionBody } = question;
+    const { helpfulness } = this.state;
+    const { question: { question_body: questionBody } } = this.props;
+
     return (
       <div className="question-list-entry">
         <div className="question">
@@ -52,9 +85,9 @@ class QuestionListEntry extends React.Component {
           <div className="question-data">
             <p>
               Helpful?
-              <a href="/"> Yes </a>
+              {this.renderHelpfulButton()}
               (
-              {questionHelpfulness}
+              {helpfulness}
               )
             </p>
             <button type="button" onClick={this.toggleAnswerModal}>Add Answer</button>
