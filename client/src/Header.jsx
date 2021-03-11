6424734ca8698daee869acd2784c8d259f7c2d85
Colleen Handel <handelc22@gmail.com>
2021-03-10 23:21:38 -0600

    Implemented theme change button

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Header = ({ cart }) => (
-  <div id="header">
+const Header = ({ cart, changeTheme, darkMode }) => (
+  <div id="header" className={darkMode ? 'dark-mode' : ''}>
     <span>Catwalk</span>
-    <button id="home" type="button">
-      <Link to="/" style={{ color: '#fff' }}>
-        <i className="fas fa-home" />
-      </Link>
+    <button id="dark-mode-button" type="button" onClick={changeTheme}>
+      <i className="fas fa-moon" style={{ display: darkMode ? 'none' : 'block' }} />
+      <i className="fas fa-sun" style={{ display: darkMode ? 'block' : 'none' }} />
     </button>
     <button id="cart" type="button">
       <i className="fas fa-shopping-cart">
@@ -17,11 +16,18 @@ const Header = ({ cart }) => (
         </span>
       </i>
     </button>
+    <button id="home" type="button">
+      <Link to="/" style={{ color: '#d1d2cd' }}>
+        <i className="fas fa-home" />
+      </Link>
+    </button>
   </div>
 );
 
 Header.propTypes = {
   cart: PropTypes.instanceOf(Array).isRequired,
+  changeTheme: PropTypes.func.isRequired,
+  darkMode: PropTypes.bool.isRequired,
 };
 
 export default Header;
